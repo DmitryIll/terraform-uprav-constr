64ca9e2146efd8d248700b656700cae313c1d503
DmitryIll <dnillarionov@gmail.com>
2024-04-15 09:15:39 +0300

    1

--- src/variables.tf
+++ src/variables.tf
@@ -1,16 +1,19 @@
 ###cloud vars
 variable "token" {
   type        = string
+  default = "${file("/root/yatoken")}"
   description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
 }
 
 variable "cloud_id" {
   type        = string
+  default = "${file("/root/cloudid")}"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
 }
 
 variable "folder_id" {
   type        = string
+  default = "${file("/root/folderid")}"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
 }
 
